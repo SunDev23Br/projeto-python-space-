f37d0a9c0554eb8c1d7967f4fd09621f7b5f0248
SunDev23Br <juliocnsj22@gmail.com>
2026-05-26 18:00:11 -0300

    main parte 1

---
+++ codigo/main.py
@@ -0,0 +1,22 @@
+print("\n<<<<<<<<< SPACE GUARDIAN >>>>>>>>>>>\n")
+print("Bem-vindo ao Space Guardian, o sistema de proteção espacial!")
+
+
+def ativar_sistema():
+
+    detritos = [
+        {"id_detrito":"satelite_desativado",
+        "coordenadas_xyz":[1500,200,400],
+        "velocidade": 5000,},
+
+        {"id_detrito":"lixo_espacial",
+        "coordenadas_xyz":[3000,500,100],
+        "velocidade": 2000,},
+        {"id_detrito":"satelite_desativado",
+        "coordenadas_xyz":[1000,300,200],
+        "velocidade": 3000,}
+
+
+    ]
+
+    return detritos
